d167be77198b265f61a60bd8700eea25e3a40777
Roy Straub <roy.straub@codesquad.nl>
2021-02-13 10:29:20 +0100

    program outline

--- src/sandwich_maker.py
+++ src/sandwich_maker.py
@@ -1,3 +1,14 @@
 #! python3
 
-print('hello')
+import pyinputplus as pyip
+
+# get bread type (wheat, white, sourdough)
+
+# get protein type (chicken, turkey, ham, tofu)
+
+# want cheese?
+# if so which cheese? (cheddar, swiss, mozarella)
+# Want other toppings? (mayo, mustard, lettuce, tomato)
+# How many do you want?
+
+# Show the total price
